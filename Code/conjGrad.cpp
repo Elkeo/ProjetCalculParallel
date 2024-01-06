c327286dbfb1422b7c53fc1a715af7772083404c
ziz147 <ziadzahi81@gmail.com>
2024-01-06 13:38:09 +0100

    BiCGstab

--- Code/conjGrad.cpp
+++ Code/conjGrad.cpp
@@ -47,3 +47,66 @@ valarray<double> conjugateGradient(const valarray<double>& RightHandSide, const 
    }
    return x;
 }
+
+
+
+valarray<double> BiCGstab(const valarray<double>& RightHandSide, const procData& proc, const SpaceTimeDomain& dom, double epsilon, int kmax)
+{
+   /*Calcul de la solutiona avec le solveur Bi gradient conjugué stabilisé*/
+
+   size_t N = RightHandSide.size();
+   valarray<double> x(0.0, N), r(0.0, N), r0(0.0, N), p(0.0, N), v(0.0, N), s(0.0, N), t(0.0, N);
+   double alpha(1.0), beta(0.0), omega(1.0), rho(1.0), rhoPrev(1.0);
+   int k(0);
+
+   // Calcule r0 = b - Ax0
+   r = RightHandSide;
+   r0 = r;
+   // Initialise p, v, s, t
+   p = r;
+   v = r;
+   s = r;
+   t = r;
+
+   // Norme du résidu initial
+   rho = (r0 * r).sum();
+   beta = sqrt(rho);
+
+   while ((beta > epsilon) and (k < kmax))
+   {
+      rho = (r0 * r).sum();
+      beta = (rho / rhoPrev) * (alpha / omega);
+      p = r + beta * (p - omega * v);
+
+      v = prodMatvect(p, proc, dom);
+
+      
+      alpha = rho / (r0 * v).sum();
+
+      s = r - alpha * v;
+
+      t = prodMatvect(s, proc, dom);
+
+      omega = (t * s).sum() / (t * t).sum();
+
+      // Mise à jour de la solution x, du résidu r
+      x = x + alpha * p + omega * s;
+      r = s - omega * t;
+
+
+      rhoPrev = rho;
+      beta = sqrt(rho);
+
+      k++;
+   }
+
+
+   if (k >= kmax)
+   {
+      printf("Tolérance non-atteinte : %g.", beta);
+   }
+
+   return x;
+
+
+}
--- Code/conjGrad.h
+++ Code/conjGrad.h
@@ -2,3 +2,4 @@
 #include "main.h"
 
 std::valarray<double> conjugateGradient(const std::valarray<double>& RightHandSide, const procData& proc, const SpaceTimeDomain& dom, double epsilon = 1.0e-8, int kmax = 1000);
+std::valarray<double> BiCGstab(const valarray<double>& RightHandSide, const procData& proc, const SpaceTimeDomain& dom, double epsilon, int kmax);
